bbc46b064fccd527cb30560258e885b674f76775
aletson <me@ajl.io>
2019-06-24 14:13:54 -0400

    put instead of putItem

--- post/index.js
+++ post/index.js
@@ -79,7 +79,7 @@ exports.handler = function(event,context,callback) {
         }
       };
     }
-    dynamoClient.putItem(params, function(err, data) {
+    dynamoClient.put(params, function(err, data) {
       if(err) {
         console.log('Error: ', err);
       } else {
