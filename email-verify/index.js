db30d445e0c3548569e208ef92d5c071beb89a33
aletson <me@ajl.io>
2019-06-24 15:31:46 -0400

    index config; fix verification system

--- email-verify/index.js
+++ email-verify/index.js
@@ -2,26 +2,43 @@ var AWS = require('aws-sdk');
 
 exports.handler = function(event,context,callback) {
   var dynamoClient = new AWS.DynamoDB.DocumentClient()
-  
   var params = {
-    TableName: 'comments',
-    Key: {
-      'approval_uuid': event.queryStringParameters.uid
-    },
-    UpdateExpression: "remove approval_uuid",
-    ReturnValues: "ALL_NEW"
-  };
-  dynamoClient.update(params, function(err, data) {
-    if (err) {
-      console.error('Update failed: ', JSON.stringify(err, null, 2));
-    } else {
-      var response = {
-        "isBase64Encoded": false,
-        "headers": {"Content-Type": "text/html"},
-        "statusCode": 200,
-        "body": '<html><head><title>Comment Approved - ajl.io</title></head><body>Approval successful! This window can now be closed.</body></html>;'
-	    };
-      callback(err, response);
+	  TableName: "comments",
+    IndexName: "approval_uuid-index",
+	  KeyConditionExpression: "approval_uuid = :uuid",
+    ExpressionAttributeValues: {
+      ":uuid": event.queryStringParameters.uid
     }
-  });
+  };
+    
+  dynamoClient.query(params, function(err, data) {
+    if(err) {
+      console.error('Query failed: ', JSON.stringify(err, null, 2));
+    } else {
+      data.Items.forEach(function(comment) {
+        var params = {
+          TableName: 'comments',
+          Key: {
+            "post_uid": comment.post_uid,
+            "sortKey": comment.sortKey
+          },
+          UpdateExpression: "remove approval_uuid",
+          ReturnValues: "ALL_NEW"
+        };
+        dynamoClient.update(params, function(err, data) {
+          if (err) {
+            console.error('Update failed: ', JSON.stringify(err, null, 2));
+          } else {
+            var response = {
+              "isBase64Encoded": false,
+              "headers": {"Content-Type": "text/html"},
+              "statusCode": 200,
+              "body": '<html><head><title>Comment Approved - ajl.io</title></head><body>Approval successful! This window can now be closed.</body></html>'
+	          };
+            callback(err, response);
+          }
+        });
+      });
+    }
+  });    
 };
